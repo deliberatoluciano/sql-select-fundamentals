1978ba268e6c0f4958aeb1993762769cba0c9657
deliberatoluciano <145874596+deliberatoluciano@users.noreply.github.com>
2026-09-24 17:18:56 -0300

    Update consultas_basicas.sql

--- consultas_basicas.sql
+++ consultas_basicas.sql
@@ -1,1 +1,11 @@
-
+-- ══════════════════════════════════════════
+-- TechStore — Consultas Básicas SELECT
+-- Autor: Luciano De Liberato
+-- Fecha: 29-09-2026
+-- ══════════════════════════════════════════
+-- Consulta 1: Exploración general de la tabla sales
+select * from sales; --SELECT * sirve cuando necesitás consultar rápidamente todas las columnas de una tabla, especialmente en pruebas o exploración; en consultas finales, es mejor indicar solo las columnas necesarias.
+-- Consulta 2: Selección de columnas específicas para finanzas
+select customer_id, product_id, total_amount from sales;
+-- Consulta 3: Selección con alias en español para stakeholders
+select order_date as fecha_pedido, product_name as nombre_producto, quantity as cantidad_unidades from sales;
